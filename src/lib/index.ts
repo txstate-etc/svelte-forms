faf118b2765419d6e0482681d5998ca9e6abc309
Nick Wing <nwing@txstate.edu>
2022-01-19 16:22:23 -0600

    prep for initial npm publish

---
+++ src/lib/index.ts
@@ -0,0 +1,5 @@
+export { default as AddMore } from './AddMore.svelte'
+export { default as Field } from './Field.svelte'
+export { default as Form } from './Form.svelte'
+export { default as SubForm } from './SubForm.svelte'
+export * from './FormStore.js'
