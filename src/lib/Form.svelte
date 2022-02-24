ceef117bc7b0ec3fa7140d18537a4d62cb27f5cb
Nick Wing <nwing@txstate.edu>
2022-02-24 11:21:25 -0600

    switch to elementqueries action from svelte-components instead of the rushed local version

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import { resize } from '@txstate-mws/svelte-components'
-  import type { ElementSize } from '@txstate-mws/svelte-components'
+  import { eq } from '@txstate-mws/svelte-components'
   import { onMount, setContext } from 'svelte'
   import { FormStore, FORM_CONTEXT } from '$lib/FormStore'
   import type { Feedback, SubmitResponse } from '$lib/FormStore'
@@ -21,14 +20,6 @@
     if (resp.success) await success?.()
   }
 
-  const breakpoints = [1600, 1500, 1400, 1300, 1200, 1100, 1000, 900, 800, 700, 600, 500, 400, 300, 200, 100]
-  let dataeq = ''
-  function onResize (e: UIEvent & { currentTarget: EventTarget & HTMLFormElement, detail: ElementSize }) {
-    const rounded = Math.floor(e.detail.clientWidth / 100) * 100
-    dataeq = breakpoints.filter(b => b > rounded).map(b => b + 'px').join(' ')
-    if ($store.width !== rounded) store.update(v => ({ ...v, width: rounded }))
-  }
-
   let form
   onMount(() => {
     if (preload != null) store.setData(preload)
@@ -39,11 +30,10 @@
       attributes: false,
       characterData: false
     })
-    onResize({ detail: { clientWidth: form.clientWidth } } as any)
     return () => mutationobserver.disconnect()
   })
 </script>
 
-<form bind:this={form} {name} class={className} on:submit|preventDefault={onSubmit} use:resize on:resize={onResize} data-eq={dataeq} {autocomplete}>
+<form bind:this={form} {name} class={className} on:submit|preventDefault={onSubmit} use:eq={{ store }} {autocomplete}>
   <slot messages={$store.messages.global} allMessages={$store.messages.all} saved={$store.saved} validating={$store.validating} submitting={$store.submitting} valid={$store.valid} invalid={$store.invalid} />
 </form>
