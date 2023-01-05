0fe4408959d207e8be72d0185307836334cda716
Nick Wing <nwing@txstate.edu>
2023-01-05 15:50:25 -0600

    better type for data slot prop on Form, release 1.3.3

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -4,7 +4,7 @@
   import { FormStore, FORM_CONTEXT } from '$lib/FormStore'
   import type { Feedback, SubmitResponse } from '$lib/FormStore'
 
-  type T = $$Generic
+  type T = $$Generic<any>
 
   interface $$Events {
     saved: CustomEvent<T>
